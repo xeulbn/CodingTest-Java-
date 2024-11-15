450edad79ecae13be70dc63c82e02610d44cfebe
xeulbn <gsbtiger0215@naver.com>
2024-11-15 23:52:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 18844 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int K;
+
+    static int visited[] = new int[100001];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st  = new StringTokenizer(br.readLine()," ");
+
+        N=Integer.parseInt(st.nextToken());
+        K=Integer.parseInt(st.nextToken());
+
+        if(N==K){
+            System.out.println(0);
+        }else{
+            bfs(N);
+        }
+
+
+
+
+    }
+
+    public static void bfs(int x) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(x);
+        visited[x]=1;
+
+        while (!queue.isEmpty()) {
+            int tmp = queue.poll();
+
+            for(int i=0;i<3;i++){
+                int next;
+                if(i==0){
+                    next=tmp+1;
+                }else if(i==1){
+                    next=tmp-1;
+                }else{
+                    next=tmp*2;
+                }
+
+                if(next==K){
+                    System.out.println(visited[tmp]);
+                    return;
+                }
+
+                if(next>=0&& next<visited.length&&visited[next]==0){
+                    queue.add(next);
+                    visited[next]=visited[tmp]+1;
+                }
+            }
+
+        }
+    }
+}
