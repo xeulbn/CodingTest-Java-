c911a551574a16afb8181294f16cd367ea41fbcd
xeulbn <gsbtiger0215@naver.com>
2025-02-03 11:47:57 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 96.14 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Solution {
+    
+    static int n;
+    static int m;
+    static int[] oil;
+    
+    public int solution(int[][] land) {
+        int answer = 0;
+        n=land.length;
+        m= land[0].length;
+        
+        oil = new int[m];
+        
+        boolean[][] visited = new boolean[n][m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(land[i][j]==1&&visited[i][j]==false){
+                    bfs(land,visited,i,j);
+                }
+            }
+        }
+        
+        answer= Arrays.stream(oil).max().getAsInt();
+        return answer;
+    }
+    
+    public void bfs(int[][]land, boolean[][]visited, int x, int y){
+        Queue<int[]> q= new LinkedList<>();
+        q.add(new int[]{x,y});
+        visited[x][y]=true;
+        
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        
+        int count=1;
+        Set<Integer> set= new HashSet<>();
+        
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            set.add(now[1]);
+            
+            for(int i=0;i<4;i++){
+                int nx= now[0]+dx[i];
+                int ny= now[1]+dy[i];
+                
+                if(nx<0||nx>=n||ny<0||ny>=m){
+                    continue;
+                }
+                
+                if(land[nx][ny]==1&&visited[nx][ny]==false){
+                    q.add(new int[]{nx,ny});
+                    visited[nx][ny]=true;
+                    count+=1;
+                }
+            }
+        }
+        for(int index:set){
+            oil[index]+=count;
+        }
+    }
+}
