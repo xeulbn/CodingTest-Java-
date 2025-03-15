252b3ea05f7a11dde106ee12e76637d3d0252721
xeulbn <gsbtiger0215@naver.com>
2025-03-15 18:58:27 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 17.95 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String str1, String str2) {
+
+        List<String> list1 = stringSplit(str1);
+        List<String> list2 = stringSplit(str2);
+
+
+        if (list1.isEmpty() && list2.isEmpty()) {
+            return 65536;
+        }
+
+        List<String> union = new ArrayList<>();	
+        List<String> inter = new ArrayList<>();	
+
+
+        for (String s : list1) {
+
+            if (list2.remove(s)) {
+                inter.add(s);
+            }
+
+
+            union.add(s);
+        }
+
+
+        union.addAll(list2);
+
+
+        double jacquard = (double) inter.size() / (double) union.size();
+
+        return (int) (jacquard * 65536);
+    }
+
+
+    private List<String> stringSplit(String str) {
+        List<String> list = new ArrayList<>();
+        str = str.toLowerCase(); 
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            String s = str.substring(i, i + 2).replaceAll("[^a-z]", "");
+
+            if (s.length() < 2) {
+                continue;
+            }
+
+            list.add(s);
+        }
+
+        return list;
+    }
+}
