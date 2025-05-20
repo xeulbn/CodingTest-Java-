3089a9d9ba6ea37a010554a6bb7f6da2a7211297
xeulbn <gsbtiger0215@naver.com>
2025-05-20 23:54:30 +0900

    [level 3] Title: 징검다리 건너기, Time: 56.56 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -3,38 +3,48 @@ import java.util.*;
 class Solution {
     
     public int solution(int[] stones, int k) {
-        int answer=0;
-        int min=1;
-        int max=200000000;
+        int answer = 0;
+        int people=1;
         
-        while(min<=max){
-            int mid= (min+max)/2;
-            if(checkCango(stones,k,mid)){
-                min=mid+1;
-                answer=Math.max(answer,mid);
-            }else{
-                max=mid-1;
+        int size= stones.length;
+        int index=0;
+        
+        while(people!=0){
+            int go = checkCango(stones,k,index);
+            answer=people;
+            if(go==0){
+                people=0;
+                break;
+            }
+            stones[index]=stones[index]-1;
+            
+            index+=go;
+            if(index>=size){
+                people++;
+                index=0;
             }
         }
         
         return answer;
     }
     
-    public static boolean checkCango(int[] stones ,int k,int friends){
-        int skip=0;
+    public static int checkCango(int[] stones ,int limit,int now){
+        int noCnt=0;
+        int indexCnt=0;
         
-        for (int stone:stones){
-            if(stone<friends){
-                skip++;
+        for(int i=1;i<=limit;i++){
+            if(stones[now+i]==0){
+                noCnt++;
             }else{
-                skip=0;
-            }
-            
-            if(skip==k){
-                return false;
+                indexCnt=i;
+                break;
             }
         }
         
-        return true;
+        if(indexCnt==limit){
+            return 0;
+        }else{
+            return indexCnt;
+        }
     }
 }
