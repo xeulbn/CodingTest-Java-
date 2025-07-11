acb6f6351c2d5570665c3d09068f9d1d3a294bbd
xeulbn <gsbtiger0215@naver.com>
2025-07-11 14:59:23 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,26 +1,38 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static int t;
     public static int n;
-    public static int[] dp;
+    public static int dp[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        t= Integer.parseInt(br.readLine());
-        dp= new int[11];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        t= Integer.parseInt(st.nextToken());
+        dp=new int[12];
+
+        dp[0]=0;
         dp[1]=1;
         dp[2]=2;
         dp[3]=4;
+        solution(11);
 
-        for(int i=4;i<11;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-        }
         for(int i=0;i<t;i++){
-            n= Integer.parseInt(br.readLine());
+            st=new StringTokenizer((br.readLine()));
+            n=Integer.parseInt(st.nextToken());
             System.out.println(dp[n]);
         }
 
+    }
+
+    public static int solution(int x){
+        for(int i=4;i<=x;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+
+        return dp[x];
     }
 }
