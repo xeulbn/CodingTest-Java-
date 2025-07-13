6a66ce22c8021e64f1c63a7246a779f2a41b8c45
xeulbn <gsbtiger0215@naver.com>
2025-07-13 20:44:48 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -2,25 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static int n;
     public static int[][] arr;
     public static int[][] dp;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        n=Integer.parseInt(br.readLine());
-        arr=new int[n+1][3];
+        n= Integer.parseInt(br.readLine());
+
+        arr=new int [n+1][3];
+        dp=new int[n+1][3];
 
         for(int i=0;i<n;i++){
-            StringTokenizer st= new StringTokenizer(br.readLine());
+            st= new StringTokenizer(br.readLine());
+
             arr[i][0]=Integer.parseInt(st.nextToken());
             arr[i][1]=Integer.parseInt(st.nextToken());
             arr[i][2]=Integer.parseInt(st.nextToken());
         }
 
-        dp=new int[n+1][3];
         dp[0][0]=arr[0][0];
         dp[0][1]=arr[0][1];
         dp[0][2]=arr[0][2];
@@ -31,8 +33,8 @@ public class Main {
             dp[i][2]=Math.min(dp[i-1][0],dp[i-1][1])+arr[i][2];
         }
 
-        System.out.println(Math.min(dp[n][0],Math.min(dp[n][1],dp[n][2])));
-
+        int answer= Math.min(Math.min(dp[n][0],dp[n][1]),dp[n][2]);
+        System.out.println(answer);
 
 
     }
