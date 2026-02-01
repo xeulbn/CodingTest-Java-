7ced9fe1a3f6c5a5e99dbb9d609006ab684afe82
xeulbn <gsbtiger0215@naver.com>
2026-02-01 12:51:37 +0900

    [level 2] Title: 소수 찾기, Time: 9.44 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,50 +1,63 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
+    
+    public int solution(String numbers){
+        int[] number = new int[numbers.length()];
 
-        static ArrayList<Integer> arr = new ArrayList<>();
-        static boolean[] check = new boolean[7];
-
-        public int solution(String numbers){
-            int answer = 0;
-            for(int i=0; i<numbers.length(); i++){
-                dfs(numbers,"",i+1);
-            }
-
-            for(int i=0; i<arr.size(); i++){
-                if(isPrime(arr.get(i))) answer++;
-            }
-
-            return answer;
+        for(int i=0;i<numbers.length();i++){
+            number[i] = numbers.charAt(i) - '0';
         }
 
-        public void dfs(String numbers, String s, int depth) {
-            if(s.length() == depth){
-                int num = Integer.parseInt(s);
-                if(!arr.contains(num)){
-                    arr.add(num);
-                }
-            }
-
-            for(int i=0; i<numbers.length(); i++){
-                if(!check[i]){
-                    check[i] = true;
-                    s += numbers.charAt(i);
-                    dfs(numbers, s, depth);
-                    check[i] = false;
-                    s = s.substring(0, s.length()-1);
-                }
-            }
-
+        boolean[] visited = new boolean[number.length];
+        Set<Integer> made = new HashSet<>();          
+        dfs(number, visited, new ArrayList<>(), made);
+        
+        int count = 0;
+        for (int x : made) {
+            if (isPrime(x)) count++;
         }
+        return count;
+    }
 
-        public static boolean isPrime(int n){
-            if(n<2) return false;
-
-            for(int i=2; i*i<=n; i++) {
-                if(n % i == 0) return false;
+    public void dfs(int[] number, boolean[] visited, List<Integer> curr, Set<Integer> made){
+        if(!curr.isEmpty()){
+            int val = 0;
+            for(int d : curr){
+                val = val * 10 + d;
             }
+            made.add(val);
+        }
+        
+        for(int i=0;i<number.length;i++){
+            if(visited[i]) 
+                continue;
 
-            return true;
+            visited[i] = true;
+            curr.add(number[i]);
+
+            dfs(number, visited, curr, made);
+
+            curr.remove(curr.size()-1);
+            visited[i] = false;
         }
     }
+    
+    private boolean isPrime(int n) {
+        if (n < 2) 
+            return false;
+        
+        if (n == 2) 
+            return true;
+        
+        if (n % 2 == 0) 
+            return false;
+        
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) 
+                return false;
+        }
+        return true;
+    }
+    
+}
