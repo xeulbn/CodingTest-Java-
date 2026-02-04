17cf1528091acd9d153106514fbee36a9d21358f
xeulbn <gsbtiger0215@naver.com>
2026-02-04 19:22:24 +0900

    [Silver IV] Title: 설탕 배달, Time: 92 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -7,31 +7,26 @@ class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
-        int answer = findMinimum(n);
+        int answer= findMinimum(n);
         System.out.println(answer);
-        
     }
 
     public static int findMinimum(int n){
         int[] dp = new int[n+1];
-        
-        if(n>=3){
+        if(n>=3)
             dp[3]=1;
-        }
-        if(n>=5){
+        if(n>=5)
             dp[5]=1;
-        }
-
+        
         for(int i=6;i<=n;i++){
             if(i%5==0){
                 dp[i]=dp[i-5]+1;
             }else if(i%3==0){
                 dp[i]=dp[i-3]+1;
             }else{
-                if(dp[i-3] !=0 && dp[i-5]!=0){
-                    dp[i]=Math.min(dp[i-3], dp[i-5])+1;
+                if(dp[i-3]!=0 && dp[i-5]!=0){
+                    dp[i]=Math.min(dp[i-3],dp[i-5])+1;
                 }
             }
         }
