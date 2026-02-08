b6d73309204f30555a06d55e7700ce606d91f6c7
xeulbn <gsbtiger0215@naver.com>
2026-02-08 14:35:33 +0900

    [Gold IV] Title: 공유기 설치, Time: 376 ms, Memory: 40516 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static int[] arr;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n =Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        arr=new int[n];
+        for(int i=0;i<n;i++){
+            st=new StringTokenizer(br.readLine());
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int answer = upperBound(c);
+        System.out.println(answer);
+    }
+
+    public static int upperBound(int c){
+        int left = 1;
+        int right = arr[arr.length-1]-arr[0];
+        int answer=0;
+
+        while(left<=right){
+            int mid = (left+right)/2;
+
+            if(canInstall(mid)>=c){
+                answer=mid;
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return answer;
+    }
+
+    public static int canInstall(int mid){
+        int count =1;
+        int lastIndex=arr[0];
+        
+        for(int i=1;i<arr.length;i++){
+            int index=arr[i];
+
+            if(index-lastIndex >= mid){
+                count++;
+                lastIndex=index;
+            }
+        }
+        return count;
+    }
+}
