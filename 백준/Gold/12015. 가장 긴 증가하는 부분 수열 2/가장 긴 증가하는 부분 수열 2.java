df7f7ef50a6e85ecfd2c796beede008baf56813d
xeulbn <gsbtiger0215@naver.com>
2026-02-10 18:42:40 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 480 ms, Memory: 96196 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main {
+
+    public static int n;
+    public static int[] arr;
+    public static int[] lis;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n=Integer.parseInt(st.nextToken());
+        arr= new int[n];
+        lis = new int[n];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        lis[0]=arr[0];
+        int lisLength = 1;
+        for(int i=1;i<arr.length;i++){
+            int key = arr[i];
+            
+            if(lis[lisLength-1]<key){
+                lisLength++;
+                lis[lisLength-1]=key;
+            }else{
+                int left=0;
+                int right=lisLength;
+                while(left<right){
+                    int mid = (left+right)/2;
+
+                    if(lis[mid]<key){
+                        left=mid+1;
+                    }else{
+                        right=mid;
+                    }
+                }
+                lis[left]=key;
+            }
+        }
+        System.out.println(lisLength);
+    }
+    
+}
