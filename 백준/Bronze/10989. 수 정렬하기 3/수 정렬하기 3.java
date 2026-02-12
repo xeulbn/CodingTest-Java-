afdb16cc8480d91bb299b9d3f426af234e2a0a42
xeulbn <gsbtiger0215@naver.com>
2026-02-12 20:41:20 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2528 ms, Memory: 353024 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main {
+
+    public static int n;
+    public static int[] arr;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        n= Integer.parseInt(st.nextToken());
+        arr=new int[n];
+        for(int i=0;i<n;i++){
+            st=new StringTokenizer(br.readLine());
+            arr[i]= Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb= new StringBuilder();
+        for(int value : arr){
+            sb.append(value).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+    
+}
