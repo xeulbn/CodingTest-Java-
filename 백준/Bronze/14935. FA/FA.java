15e5d17a5e228e126962e2f6fa884b630a64a567
xeulbn <gsbtiger0215@naver.com>
2026-02-25 09:47:15 +0900

    [Bronze IV] Title: FA, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
+        System.out.println("FA");
+    }
+}
