e1cd0816504d44b9ad8c02931ede6fe95c2b235a
xeulbn <gsbtiger0215@naver.com>
2026-03-14 19:27:45 +0900

    [Gold IV] Title: 스도쿠, Time: 376 ms, Memory: 19208 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -2,86 +2,97 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
-    private static final int SUDOKU_ROW_SIZE = 9;
-    private static final int SUDOKU_COL_SIZE = 9;
-    public static int[][] sudoku;
-    public static List<int[]> empties = new ArrayList<>();
-    private static boolean solved= false;
+    public static int[][] sdoku;
+    public static List<Node> empties;
+    public static boolean solved = false;
 
+    public static class Node{
+        int x;
+        int y;
+        
+        public Node(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        sudoku=new int[SUDOKU_ROW_SIZE][SUDOKU_COL_SIZE];
-        for(int i=0;i<SUDOKU_ROW_SIZE;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0;j<SUDOKU_COL_SIZE;j++){
-                sudoku[i][j]= Integer.parseInt(st.nextToken());
-                if(sudoku[i][j]== 0){
-                    empties.add(new int[]{i,j});
+        sdoku=new int[9][9];
+        empties=new ArrayList<>();
+        for(int i=0;i<9;i++){
+            st= new StringTokenizer(br.readLine());
+            for(int j=0;j<9;j++){
+                sdoku[i][j]=Integer.parseInt(st.nextToken());
+                if(sdoku[i][j]==0){
+                    empties.add(new Node(i,j));
                 }
             }
         }
-        
-        dfs(0);
 
-        StringBuilder sb = new StringBuilder();
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                sb.append(sudoku[r][c]).append(' ');
+        solveSdoku(0);
+
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                sb.append(sdoku[i][j]).append(' ');
             }
             sb.append('\n');
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
-    public static void dfs(int index){
+    public static void solveSdoku(int depth){
         if(solved){
             return;
         }
 
-        if(index==empties.size()){
+        if(depth==empties.size()){
             solved=true;
             return;
         }
 
-        int[] pos = empties.get(index);
-        int row = pos[0];
-        int col = pos[1];
+        Node pos = empties.get(depth);
+        int row = pos.x;
+        int col = pos.y;
 
         for(int i=1;i<=9;i++){
             if(isValid(row,col,i)){
-                sudoku[row][col]=i;
-                dfs(index+1);
+                sdoku[row][col]=i;
+                solveSdoku(depth+1);
                 if(solved){
                     return;
                 }
-                sudoku[row][col]=0;
+                sdoku[row][col]=0;
             }
         }
     }
 
     public static boolean isValid(int row, int col, int num){
         for(int x=0;x<9;x++){
-            if(sudoku[row][x]==num){
+            if(sdoku[row][x]==num){
                 return false;
             }
         }
 
         for(int x=0;x<9;x++){
-            if(sudoku[x][col]==num){
+            if(sdoku[x][col]==num){
                 return false;
             }
         }
 
-        int sr = (row/3)*3;
+        int sr= (row/3)*3;
         int sc = (col/3)*3;
-        
+
         for(int i=sr;i<sr+3;i++){
             for(int j=sc;j<sc+3;j++){
-                if(sudoku[i][j]==num)
+                if(sdoku[i][j]==num){
                     return false;
+                }
             }
         }
 
