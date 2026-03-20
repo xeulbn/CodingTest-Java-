35a9f783c0b2fc81cd1b82f6620e539221169440
xeulbn <gsbtiger0215@naver.com>
2026-03-20 11:22:23 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18184 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,28 +1,30 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
-import java.util.*;;
 
-public class Main {
+class Main {
 
-    static int n;
-    static int[] dp;
+    public static int n;
+    public static int[] dp;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        
+        n= Integer.parseInt(st.nextToken());
+        dp = new int[n+1];
+        dp[1] = 0;
 
-    public static void main(String[] args)throws IOException {
-        Scanner sc = new Scanner(new InputStreamReader(System.in));
-        n=sc.nextInt();
-        dp=new int[n+1];
-
-        dp[1]=0;
         for(int i=2;i<n+1;i++){
             dp[i]=dp[i-1]+1;
-            if(i%2==0&&dp[i]>dp[i/2]+1){
-                dp[i]=dp[i/2]+1;
+            if(i%2==0){
+                dp[i]= Math.min(dp[i],dp[i/2]+1);
             }
-            if(i%3==0&&dp[i]>dp[i/3]+1){
-                dp[i]=dp[i/3]+1;
+            if(i%3==0){
+                dp[i]=Math.min(dp[i],dp[i/3]+1);
             }
         }
+
         System.out.println(dp[n]);
-
     }
-
 }
