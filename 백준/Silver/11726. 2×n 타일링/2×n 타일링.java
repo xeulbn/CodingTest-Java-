63ef71df3b1d716f7ea69977a15930f3ea88d817
xeulbn <gsbtiger0215@naver.com>
2026-03-20 11:53:30 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,21 +1,38 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+class Main {
 
     public static int n;
-    public static int[] dp;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        
+        n=Integer.parseInt(st.nextToken());
+        
+        int answer= findAnswer(n);
+        
+        System.out.println(answer);
+        
+    }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        dp=new int[n+1];
-
-        dp[0]=1;
+    public static int findAnswer(int x){
+        if(x==1){
+            return 1;
+        }
+        if(x==2){
+            return 2;
+        }
+        
+        int[] dp= new int[x+1];
         dp[1]=1;
-        for(int i=2;i<=n;i++){
+        dp[2]=2;
+
+        for(int i=3;i<x+1;i++){
             dp[i]=(dp[i-1]+dp[i-2])%10007;
         }
-        System.out.println(dp[n]);
-
+        return dp[x];
     }
 }
