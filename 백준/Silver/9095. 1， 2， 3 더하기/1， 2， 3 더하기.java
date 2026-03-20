93e376b356703c3c7a4919801e683da60306f6b0
xeulbn <gsbtiger0215@naver.com>
2026-03-20 11:37:11 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,38 +1,40 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+class Main {
 
     public static int t;
-    public static int n;
-    public static int dp[];
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        t= Integer.parseInt(st.nextToken());
-        dp=new int[12];
-
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=4;
-        solution(11);
-
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        
+        t=Integer.parseInt(st.nextToken());
         for(int i=0;i<t;i++){
-            st=new StringTokenizer((br.readLine()));
-            n=Integer.parseInt(st.nextToken());
-            System.out.println(dp[n]);
+            st= new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int answer = dp(n);
+            System.out.println(answer);
         }
-
     }
 
-    public static int solution(int x){
-        for(int i=4;i<=x;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+    public static int dp(int x){
+        int[] dp = new int[x+1];
+        if(x==0||x==1){
+            return 1;
+        }
+        if(x==2){
+            return 2;
         }
 
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<x+1;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
         return dp[x];
     }
 }
