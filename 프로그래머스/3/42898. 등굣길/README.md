a33c291960650565582f1c65804f1807a73d2000
xeulbn <gsbtiger0215@naver.com>
2026-04-04 22:37:04 +0900

    [level 3] Title: 등굣길, Time: 0.88 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 54 MB, 시간: 1.41 ms
+메모리: 54.1 MB, 시간: 0.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 03일 15:16:41
+2026년 04월 04일 22:37:00
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,37 +2,48 @@ import java.util.*;
 
 class Solution {
     
-    public static int[][] dp;
-    public static int x=1000000007;
+    public int[][] dp;
+    public boolean[][] isPuddle;
+    public int MOD = 1000000007;
     
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        dp= new int[m+1][n+1];
         
-        for(int[] puddle: puddles){
-            dp[puddle[0]][puddle[1]]=-1;
+        dp=new int[n][m];
+        isPuddle=new boolean[n][m];
+        dp[0][0]=1;
+        
+        for (int[] puddle : puddles) {
+            int x = puddle[0] - 1; 
+            int y = puddle[1] - 1;
+            isPuddle[y][x] = true;
         }
         
-        dp[1][1]=1;
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
-                if(dp[i][j]==-1){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(isPuddle[i][j]){
                     dp[i][j]=0;
                     continue;
                 }
                 
-                if(dp[i-1][j]>0){
-                    dp[i][j]+=dp[i-1][j]%x;
-                    
+                if(i==0&&j==0){
+                    continue;
                 }
-                if(dp[i][j-1]>0){
-                    dp[i][j]+=dp[i][j-1]%x;
+                
+                int fromUp =0;
+                int fromLeft=0;
+                
+                if(i>0){
+                    fromUp=dp[i-1][j];
                 }
-                dp[i][j]=dp[i][j]%x;
+                
+                if(j>0){
+                    fromLeft=dp[i][j-1];
+                }
+                
+                dp[i][j]=(fromUp+fromLeft)%MOD;
             }
         }
         
-        answer=dp[m][n];
-        return answer;
+        return (dp[n-1][m-1])%MOD;
     }
 }
